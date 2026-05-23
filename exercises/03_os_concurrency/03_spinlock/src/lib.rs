3d328a2a9796cee6f292ad2351af8f8e09b00ade
okotohime <okotohime@outlook.com>
2026-05-23 18:04:34 +0800

    solve os_concurrency section

--- exercises/03_os_concurrency/03_spinlock/src/lib.rs
+++ exercises/03_os_concurrency/03_spinlock/src/lib.rs
@@ -10,6 +10,7 @@
 //! - `UnsafeCell` provides interior mutability
 
 use std::cell::UnsafeCell;
+use std::hint::spin_loop;
 use std::sync::atomic::{AtomicBool, Ordering};
 
 /// Basic spin lock
@@ -41,7 +42,15 @@ impl<T> SpinLock<T> {
     /// Caller must ensure `unlock` is called after using the data.
     pub fn lock(&self) -> &mut T {
         // TODO
-        todo!()
+        loop {
+            match self
+                .locked
+                .compare_exchange(false, true, Ordering::Acquire, Ordering::Relaxed)
+            {
+                Ok(_) => return unsafe { &mut *self.data.get() },
+                Err(_) => spin_loop(),
+            }
+        }
     }
 
     /// Release lock.
@@ -49,14 +58,17 @@ impl<T> SpinLock<T> {
     /// TODO: Set locked to false (using Release ordering)
     pub fn unlock(&self) {
         // TODO
-        todo!()
+        self.locked.store(false, Ordering::Release);
     }
 
     /// Try to acquire lock without spinning.
     /// Returns Some(&mut T) on success, None if lock is busy.
     pub fn try_lock(&self) -> Option<&mut T> {
         // TODO: Single compare_exchange attempt
-        todo!()
+        self.locked
+            .compare_exchange(false, true, Ordering::Acquire, Ordering::Relaxed)
+            .and_then(|_| Ok(unsafe { &mut *self.data.get() }))
+            .ok()
     }
 }
 
